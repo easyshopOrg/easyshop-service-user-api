eca17b67254d2dbac064894d37c0c0d340666563
chenyucheng <yucheng.chen@tempus.cn>
2019-08-29 10:49:21 +0800

    init commit

---
+++ src/main/java/com/cyc/easy/shop/service/user/api/TbUserService.java
@@ -0,0 +1,9 @@
+package com.cyc.easy.shop.service.user.api;
+
+import com.cyc.easy.shop.commons.domain.TbUser;
+
+import java.util.List;
+
+public interface TbUserService {
+    List<TbUser> selectAll();
+}
